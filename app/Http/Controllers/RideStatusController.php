c6795a66a48b7e1220fd3f40e5ea10a112262c08
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-10-08 21:38:45 -0300

    add new models

---
+++ app/Http/Controllers/RideStatusController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RideStatus;
+use Illuminate\Http\Request;
+
+class RideStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RideStatus $rideStatus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, RideStatus $rideStatus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RideStatus $rideStatus)
+    {
+        //
+    }
+}
